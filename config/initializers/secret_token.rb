a39b8645ff547ea58484e0cc3600ceabf7c4a087
Chad McKenna <cmckenna@24tru.com>
2013-12-20 08:00:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QualvuApp::Application.config.secret_token = '7ab219a1991f9c291ff7d9bd4c8b75c5b85905046b2c7144a079f59f4a6c5fc3e81cfc01c4f2eb73c55f9efd623164588e0d71f443583c26ebfeb01ac982a6ab'
